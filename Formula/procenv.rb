74227e709a6d311d74086848d49a7ca215e96345
henrik242 <henrik242@users.noreply.github.com>
2024-01-11 06:13:47 +0100

    procenv 1.2
    v1.2 adds helpful logging

--- Formula/procenv.rb
+++ Formula/procenv.rb
@@ -1,8 +1,8 @@
 class Procenv < Formula
   desc "Show process environment variables"
   homepage "https://github.com/henrik242/procenv"
-  url "https://github.com/henrik242/procenv/archive/refs/tags/1.1.tar.gz"
-  sha256 "7371164f700c695fe27783b4471c2817e229fa7acc20d7a8b37a49687ef5f65d"
+  url "https://github.com/henrik242/procenv/archive/refs/tags/1.2.tar.gz"
+  sha256 "48b70f7a842b48098d8317f1f2f3cc1d7427537718df6561750d0ee3f502c5d0"
   license "MIT"
 
   def install
@@ -11,6 +11,6 @@ class Procenv < Formula
   end
 
   test do
-    system "#{bin}/procenv"
+    assert_equals "Unaccessible or missing PID: 1234567890", shell_output("#{bin}/procenv 1234567890").strip
   end
 end
