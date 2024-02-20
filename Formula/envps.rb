26db8843868016bf5c12e994b590a6c425e8fa9c
henrik242 <henrik242@users.noreply.github.com>
2024-02-20 19:03:51 +0100

    envps bottles

--- Formula/envps.rb
+++ Formula/envps.rb
@@ -10,6 +10,13 @@ class Envps < Formula
     bin.install "envps"
   end
 
+  bottle do
+    root_url "https://github.com/henrik242/homebrew-brew/releases/download/1.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9a060c6377222bc671f93e0e0556b61b484246a8ce2cf9ad51649af509b83801"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "84e53916852ba51134a9662c7961dd3aa19fbd3387d04d279955da3d3760d6f8"
+  end
+
   test do
     assert_equal "Unaccessible or missing PID: 1234567890", shell_output("#{bin}/envps 1234567890").strip
     assert_equal "Illegal PID: FOO", shell_output("#{bin}/envps FOO").strip
