7a5e53464aecea3c384eb8be1950de0b5bfc081f
henrik242 <henrik242@users.noreply.github.com>
2024-02-20 20:33:01 +0100

    stardates bottles

--- Formula/stardates.rb
+++ Formula/stardates.rb
@@ -11,6 +11,13 @@ class Stardates < Formula
     man1.install "stardate.1"
   end
 
+  bottle do
+    root_url "https://github.com/henrik242/homebrew-brew/releases/download/1.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d947eb7a0989eb66cd82aa15304aa7aa0d49b4c42bb09b2390d0d317159289e6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c5b3472de49343451086db70dab5fbb54829af36543a44a281e08b9e90e3dcd7"
+  end
+
   test do
     assert_equal "[-36]9350.00", shell_output("#{bin}/stardate 1970-01-01").strip
   end
