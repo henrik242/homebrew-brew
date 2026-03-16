8e3e21d071333e6b2a206809e80758788963d166
henrik242 <henrik242@users.noreply.github.com>
2026-03-16 16:16:03 +0100

    envps 2.0.1

--- Formula/envps.rb
+++ Formula/envps.rb
@@ -1,8 +1,8 @@
 class Envps < Formula
   desc "Show process environment variables"
   homepage "https://github.com/henrik242/envps"
-  url "https://github.com/henrik242/envps/archive/refs/tags/2.0.0.tar.gz"
-  sha256 "8bdf21e9234a8b188ab7b7a57ce8ba74be374096860a5c418a72fa3ef9290f36"
+  url "https://github.com/henrik242/envps/archive/refs/tags/2.0.1.tar.gz"
+  sha256 "882f4a5011e437cbcdb20d7f148dba574713dfc2094d91060474f74f54d001eb"
   license "MIT"
 
   depends_on "rust" => :build
