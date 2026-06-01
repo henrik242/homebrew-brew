d6bc4a15c62eadf268536a0858a20e3187d5307e
henrik242 <henrik242@users.noreply.github.com>
2026-06-01 11:57:36 +0200

    fix vladbot version identifier

--- Formula/vladbot.rb
+++ Formula/vladbot.rb
@@ -2,6 +2,7 @@ class Vladbot < Formula
   desc "IRC bot based on HopBot"
   homepage "https://github.com/hipdrome/vladbot"
   url "https://github.com/hipdrome/vladbot/archive/refs/tags/2.1i.tar.gz"
+  version "2.1i"
   sha256 "bb1411f6afb7f6ced61a2150c503fb2e29eddf8a694feb1eca471f56703e93e5"
   license "GPL-2.0-or-later"
 
